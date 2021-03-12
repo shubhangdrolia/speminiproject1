d1c0e7bb9d591ebdc801adba2535eb2c4b2ee7d3
shubhangdrolia <7310813051a@gmail.com>
2021-03-12 18:32:38 +0530

    while

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -70,82 +70,70 @@ public class calculator {
         Scanner scan = new Scanner(System.in);
         double a, b, c, selection;
         double d;
-        System.out.println("--------------------------Welcome---------------------------");
-        System.out.println("Please enter 0 for power");
-        System.out.println("Please enter 1 for addition");
-        System.out.println("Please enter 2 for subtraction");
-        System.out.println("Please enter 3 for multiplication");
-        System.out.println("Please enter 4 for division");
-        System.out.println("Please enter 5 for factorial");
-        System.out.println("Please enter 6 for square");
-        System.out.println("Please enter 7 for square root");
-        System.out.println("Please enter 8 for Log");
-        System.out.println("Please enter 9 for Exit");
-        selection = scan.nextInt();
-        calculator op = new calculator();
-        if (selection <= 4) {
-            System.out.println("please enter any two number");
-            a = scan.nextInt();
-            b = scan.nextInt();
+        while(true){
+            System.out.println("--------------------------Welcome---------------------------");
+            System.out.println("Please enter 0 for power");
+            System.out.println("Please enter 1 for addition");
+            System.out.println("Please enter 2 for subtraction");
+            System.out.println("Please enter 3 for multiplication");
+            System.out.println("Please enter 4 for division");
+            System.out.println("Please enter 5 for factorial");
+            System.out.println("Please enter 6 for square");
+            System.out.println("Please enter 7 for square root");
+            System.out.println("Please enter 8 for Log");
+            System.out.println("Please enter 9 for Exit");
+            selection = scan.nextInt();
+            calculator op = new calculator();
+            if (selection <= 4) {
+                System.out.println("please enter any two number");
+                a = scan.nextInt();
+                b = scan.nextInt();
 
 
-            while (selection > 4) {
-                System.out.println("Invalid input please try again");
-                System.out.println("Enter the input");
-                selection = scan.nextInt();
-            }
-            if (selection == 0) {
-                c = op.power(a, b);
-                System.out.println("The result for power is " + c);
-            }
-            else if (selection == 1) {
-                c = op.add(a, b);
-                System.out.println("The result for addition is " + c);
-            } else if (selection == 2) {
-                c = op.sub(a, b);
-                System.out.println("The result for subtraction is " + c);
-            } else if (selection == 3) {
-                c = op.mul(a, b);
-                System.out.println("The result for multiplication is " + c);
-            } else if (selection == 4) {
-                c = op.div(a, b);
-                System.out.println("The result for division is " + c);
-            }
-        }
-        else if(selection > 4 && selection <=8)
-        {
-            System.out.println("please enter any number");
-            d = scan.nextInt();
-            if(selection == 5)
-            {
-                c = op.fact(d);
-                System.out.println("The result of factorial is "+c);
-            }
+                while (selection > 4) {
+                    System.out.println("Invalid input please try again");
+                    System.out.println("Enter the input");
+                    selection = scan.nextInt();
+                }
+                if (selection == 0) {
+                    c = op.power(a, b);
+                    System.out.println("The result for power is " + c);
+                } else if (selection == 1) {
+                    c = op.add(a, b);
+                    System.out.println("The result for addition is " + c);
+                } else if (selection == 2) {
+                    c = op.sub(a, b);
+                    System.out.println("The result for subtraction is " + c);
+                } else if (selection == 3) {
+                    c = op.mul(a, b);
+                    System.out.println("The result for multiplication is " + c);
+                } else if (selection == 4) {
+                    c = op.div(a, b);
+                    System.out.println("The result for division is " + c);
+                }
+            } else if (selection > 4 && selection <= 8) {
+                System.out.println("please enter any number");
+                d = scan.nextInt();
+                if (selection == 5) {
+                    c = op.fact(d);
+                    System.out.println("The result of factorial is " + c);
+                } else if (selection == 6) {
+                    c = op.squ(d);
+                    System.out.println("The result of square is " + c);
+                } else if (selection == 7) {
+                    double x;
+                    x = op.squareroot(d);
+                    System.out.println("The result of square root is " + x);
+                } else if (selection == 8) {
+                    double x;
+                    x = op.logarithm(d);
+                    System.out.println("The result of logarithm is " + x);
+                }
 
-            else if(selection == 6)
-            {
-                c = op.squ(d);
-                System.out.println("The result of square is "+c);
+            } else if (selection == 9) {
+                System.out.println("Thank you !!");
+                break;
             }
-
-            else if(selection == 7)
-            {
-                double x;
-                x = op.squareroot(d);
-                System.out.println("The result of square root is "+x);
-            }
-
-            else if(selection == 8)
-            {
-                double x;
-                x = op.logarithm(d);
-                System.out.println("The result of logarithm is "+x);
-            }
-
-        }
-        else if(selection == 9)
-        {
-            System.out.println("Thank you !!");
         }
     }
 }
